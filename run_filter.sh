75f2d42b21ab31d40b32748c612089a0dad97e40
GuillelmoGomezCeballos <ceballos@cern.ch>
2014-08-09 12:06:58 +0200

    new

--- run_filter.sh
+++ run_filter.sh
@@ -2,8 +2,10 @@
 
 export filterMVAFile=filter_monox.C+;
 
-for dataset in `cat list_samples.txt | cut -d' ' -f1 ` ; do
-  export datasetOLD=$dataset"_mva.root"
-  echo "filling MVA information in sample: "  $dataset
+for dataset in `cat list_samples_analysis.txt | cut -d' ' -f1 ` ; do
+  export datasetOLD=$dataset"_mva.root";
+  echo "filling MVA information in sample: "  $dataset;
   root -l -q -b ${filterMVAFile}\(\"${datasetOLD}\"\);
+  #export datasetNEW=$dataset"_mva_filter.root";
+  #mv $datasetNEW $datasetOLD;
 done
